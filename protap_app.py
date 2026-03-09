a0b32d0bdba6ac60727cd08b47dc084fcb278be5
Prowrap110 <can.erden@pro-tap.com>
2026-03-09 17:18:23 +0300

    Update protap_app.py

--- protap_app.py
+++ protap_app.py
@@ -294,7 +294,7 @@ def get_plug_height(completion_type, branch_inch):
 class PDFReport(FPDF):
     def header(self):
         """This runs automatically every time a new page is created."""
-        logo_path = "protaplogo.png"
+        logo_path = "logo.png"
         
         if os.path.exists(logo_path):
             # Place the logo at the top left
